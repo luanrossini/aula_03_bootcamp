88d604e866c717f9709a7f858609ed42c56f15d3
Luan Rossini <luanrossini@gmail.com>
2024-06-13 14:39:43 -0300

    atualizando

---
+++ main.py
@@ -0,0 +1,16 @@
+x = 2
+
+if x <= 0:
+    print("e menor que zero")
+elif x <=  00:
+    print("e menor que dois")
+else:
+    print("More")
+
+
+for i in range(1,5):
+    print(i)
+
+lista_alunos = ["A","B","C"]
+for aluno in lista_alunos:
+    print(aluno)
